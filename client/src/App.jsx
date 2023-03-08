25397894ab40ab9593c4538f691d4b3a4a493eb0
Natalia Carvalho <ssspinosaure@gmail.com>
2023-03-08 19:18:06 -0300

    update

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -6,6 +6,12 @@ import Login from "./pages/Login/Login";
 import MyList from "./pages/MyList/MyList";
 import SignUp from "./pages/SignUp/SignUp";
 
+const Private = ({ Item }) => {
+  const signed = false;
+
+  return signed > 0 ? <Item /> : <Login />;
+};
+
 function App() {
   return (
     <>
@@ -13,8 +19,12 @@ function App() {
         <Routes>
           <Route>
             <Route path="/" element={<Navbar />}>
-              <Route path="/" element={<Home/>}/>
+              <Route path="/" element={<Home />} />
               <Route path="/my_list" element={<MyList />} />
+              {/* <Route
+                  path="/my_list"
+                  element={<Private Item={<MyList />} />}
+                /> */}
               <Route path="/Login" element={<Login />} />
               <Route path="/sign_up" element={<SignUp />} />
             </Route>
--- client/src/pages/MyList/MyList.jsx
+++ client/src/pages/MyList/MyList.jsx
@@ -5,7 +5,6 @@ import Footer from "../../components/Footer/Footer";
 function MyList() {
   return (
     <>
-      <div>MyList</div>
       <Power />
       <Footer />
     </>
