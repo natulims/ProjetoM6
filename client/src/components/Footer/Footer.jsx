08a99df8a0ef8b563d4a9e48db736a380e5775f6
Natalia Carvalho <ssspinosaure@gmail.com>
2023-03-11 23:30:37 -0300

    fixed footer

--- client/src/components/Footer/Footer.jsx
+++ client/src/components/Footer/Footer.jsx
@@ -1,27 +1,41 @@
 import React from "react";
-import { AnimatePresence, motion } from "framer-motion";
-import logo from "/logo2.png";
-import { fadeIn } from "../../utilities/variant";
+// import { AnimatePresence, motion } from "framer-motion";
+import logo from "/logo1.png";
+// import { fadeIn } from "../../utilities/variant";
 
 function Footer() {
   return (
     <>
-      <footer className="bg-rosy text-center w-full mt-[5rem]">
-        <motion.footer
-          variants={fadeIn("right")}
-          initial="hidden"
-          whileInView={"show"}
-          viewport={{ once: false, amount: 0.2 }}
-          className="lg:pt-0"
-        >
-          <div className="p-4 text-center">
-            <p className=" text-misty font-bold">© 2023 Copyright</p>
-            <div className="w-[150px]">
-              <img className="flex justify-center lg:ml-[52rem]" src={logo} />
-            </div>
-            <p className="text-misty font-bold">All Rights Reserved.</p>
-          </div>
-        </motion.footer>
+      <footer className="footer text-center p-4 bg-base-300 text-base-content">
+        <div className="flex justify-center">
+          <img className="w-[10rem]" src={logo} alt="" />
+        </div>
+        <div>
+          <p className="text-2xl font-bold">Copyright © 2023</p>
+        </div>
+        <div className="flex justify-center gap-2 mt-5">
+          <a
+            href="https://github.com/natulims/ProjetoM6"
+            target="_blank"
+            className="text-4xl text-rose hover:text-[#D54D44]"
+          >
+            <i className="bx bxl-github"></i>
+          </a>
+          <a
+            href="https://www.instagram.com/natulims/"
+            target="_blank"
+            className="text-4xl text-rose hover:text-[#D54D44]"
+          >
+            <i className="bx bxl-instagram"></i>
+          </a>
+          <a
+            href="https://www.deviantart.com/natulims"
+            target="_blank"
+            className="text-4xl text-rose hover:text-[#D54D44]"
+          >
+            <i className="bx bxl-deviantart"></i>
+          </a>
+        </div>
       </footer>
     </>
   );
