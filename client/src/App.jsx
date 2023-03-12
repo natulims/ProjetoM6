6dcf8ed51c3002e7eecca76cd2af7ecbe8870cf1
Natalia Carvalho <ssspinosaure@gmail.com>
2023-03-11 23:30:27 -0300

    changed routes

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,28 +1,37 @@
 import React from "react";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
+import Footer from "./components/footer/Footer";
+import Navbar from "./components/navbar/Navbar";
 import Home from "./pages/home/Home";
+import SearchAnimes from "./pages/searchAnimes/SearchAnimes";
+import Login from "./pages/login/Login"
+import MyList from "./pages/myList/MyList"
+import SignUp from "./pages/signUp/SignUp"
 
 function App() {
   return (
-   <>
-   
     <>
-      <BrowserRouter>
-        <Routes>
-          <Route>
-            {/* <Route path="/" element={<Navbar />}> */}
-              <Route path="/" element={<Home/>}/>
-              {/* <Route path="/my_list" element={<MyList />} />
-              <Route path="/Login" element={<Login />} />
-              <Route path="/sign_up" element={<SignUp />} /> */}
-            {/* </Route> */}
-          </Route>
-        </Routes>
-      </BrowserRouter>
+      <>
+        <nav>
+          <Navbar />
+        </nav>
+        <BrowserRouter>
+          <Routes>
+            <Route>
+              <Route path="/" element={<Home />} />
+              <Route path="/search" element={<SearchAnimes />} />
+              <Route path="/my_list" element={<MyList />} />
+              <Route path="/login" element={<Login />} />
+              <Route path="/sign_up" element={<SignUp />} />
+            </Route>
+          </Routes>
+        </BrowserRouter>
+        <footer>
+          <Footer />
+        </footer>
+      </>
     </>
-   
-   </>
-  )
+  );
 }
 
-export default App
+export default App;
