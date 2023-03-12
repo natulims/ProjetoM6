8ffa05fba1d73aee6417304909051dc897c411e7
Natalia Carvalho <ssspinosaure@gmail.com>
2023-03-11 23:26:47 -0300

    fixed login and sign up page

--- client/src/pages/Login/Login.jsx
+++ client/src/pages/Login/Login.jsx
@@ -1,60 +1,76 @@
 import React from "react";
-import Power from "../../components/Power/Power";
-import Footer from "../../components/Footer/Footer";
 import { Link } from "react-router-dom";
+import logo from "../../../public/logo.png";
 
 function Login() {
   return (
     <>
-      <div className="relative flex flex-col justify-center min-h-screen overflow-hidden">
-        <div className="w-full p-6 m-auto bg-red-50 rounded-md shadow-md lg:max-w-xl">
-          <h1 className="text-3xl font-semibold text-center text-mountbatten">
-            Login
-          </h1>
-          <form className="mt-6">
-            <div className="mb-2">
-              <label
-                for="email"
-                className="block text-sm font-semibold text-mountbatten"
-              >
-                Email
-              </label>
-              <input
-                type="email"
-                className="block w-full px-4 py-2 mt-2 text-rosy border rounded-md focus:border-rusty focus:ring-rusty focus:outline-none focus:ring focus:ring-opacity-40"
-              />
-            </div>
-            <div className="mb-2">
-              <label
-                for="password"
-                className="block text-sm font-semibold text-mountbatten"
-              >
-                Password
-              </label>
-              <input
-                type="password"
-                className="block w-full px-4 py-2 mt-2 text-mountbatten  border rounded-md focus:border-rusty focus:ring-rusty focus:outline-none focus:ring focus:ring-opacity-40"
-              />
-            </div>
-            <a href="#" className="text-xs text-auburn hover:underline">
-              Forget Password?
-            </a>
-            <div className="mt-6">
-              <button className="w-full px-4 py-2 tracking-wide text-misty transition-colors duration-200 transform bg-auburn rounded-md hover:bg-rosy focus:outline-none focus:bg-rusty">
-                Login
-              </button>
+      <section className="flex min-h-full items-center justify-center py-12 sm:px-6 lg:px-10">
+        <div className="w-full max-w-md space-y-8">
+          <div>
+            <img
+              className="mx-auto h-12 w-auto"
+              src={logo}
+              alt="anikaze logo"
+            />
+            <h2 className="mt-6 text-center text-3xl font-bold tracking-tight text-auburn">
+              Welcome to Anikaze!
+            </h2>
+            <p className="text-mountbatten text-center">
+              Already have an account?{" "}
+              <Link to="/sign_up" className="text-auburn">
+                Sign in
+              </Link>
+            </p>
+          </div>
+          <form className="mt-8 space-y-6">
+            <div className="rounded-md shadow-sm">
+              <div>
+                <p>Email</p>
+                <input
+                  id="email-address"
+                  name="email"
+                  type="email"
+                  autoComplete="email"
+                  required
+                  className="relative block w-full rounded-t-md border-0 py-1.5 indent-[5px] text-mountbatten placeholder:text-rose sm:text-sm sm:leading-6"
+                  placeholder="Ex.: email@email.com"
+                />
+              </div>
+              <div>
+                <p className="mx-[1px] my-[6px]">Password</p>
+                <label for="password" className="sr-only">
+                  Password
+                </label>
+                <input
+                  id="password"
+                  name="password"
+                  type="password"
+                  required
+                  className="relative block w-full rounded-b-md border-0 py-1.5 text-night ring-1 ring-inset indent-[5px] ring-gray-300 placeholder:text-mountbatten sm:text-sm sm:leading-6"
+                  placeholder="Password"
+                />
+              </div>
             </div>
 
+            <div>
+              <button
+                type="submit"
+                className="group relative flex w-full justify-center rounded-md bg-[#D54D44] py-2 px-3 text-sm font-semibold text-white hover:bg-rosy"
+              >
+                Sign in
+              </button>
+            </div>
             <div className="my-6 space-y-2">
               <button
                 aria-label="Login with Google"
                 type="button"
-                className="text-mountbatten flex items-center justify-center w-full p-2 space-x-4 border rounded-md focus:ring-2 focus:ring-offset-1 dark:border-rusty focus:ring-rusty"
+                className="text-mountbatten flex items-center justify-center w-full p-2 space-x-4 border rounded-md dark:border-rusty hover:bg-misty"
               >
                 <svg
                   xmlns="http://www.w3.org/2000/svg"
                   viewBox="0 0 32 32"
-                  className="w-5 h-5 fill-current"
+                  className="w-5 h-5 fill-[#D54D44]"
                 >
                   <path d="M16.318 13.714v5.484h9.078c-0.37 2.354-2.745 6.901-9.078 6.901-5.458 0-9.917-4.521-9.917-10.099s4.458-10.099 9.917-10.099c3.109 0 5.193 1.318 6.38 2.464l4.339-4.182c-2.786-2.599-6.396-4.182-10.719-4.182-8.844 0-16 7.151-16 16s7.156 16 16 16c9.234 0 15.365-6.49 15.365-15.635 0-1.052-0.115-1.854-0.255-2.651z"></path>
                 </svg>
@@ -62,15 +78,8 @@ function Login() {
               </button>
             </div>
           </form>
-
-          <p className="mt-8 text-xs font-light text-center text-rusty">
-            Don't have an account?
-            <Link to="/sign_up"> Register now!</Link>
-          </p>
         </div>
-      </div>
-      <Power />
-      <Footer />
+      </section>
     </>
   );
 }
--- client/src/pages/SignUp/SignUp.jsx
+++ client/src/pages/SignUp/SignUp.jsx
@@ -1,122 +1,96 @@
 import React from "react";
 import { Link } from "react-router-dom";
-import Power from "../../components/Power/Power";
-import Footer from "../../components/Footer/Footer";
+import logo from "../../../public/logo.png";
 
 function SignUp() {
   return (
-    <>
-      <div className="flex flex-col items-center min-h-screen pt-6 sm:justify-center sm:pt-0 bg-misty">
+    <section className="flex min-h-full items-center justify-center py-12 sm:px-6 lg:px-10">
+      <div className="w-full max-w-md space-y-8">
         <div>
-          <a href="/">
-            <h3 className="text-4xl font-bold text-mountbatten">Sign Up</h3>
-          </a>
-        </div>
-        <div className="w-full px-6 py-4 mt-6 overflow-hidden bg-red-50 shadow-md sm:max-w-lg sm:rounded-lg">
-          <form>
-            <div>
-              <label
-                htmlFor="name"
-                className="block text-sm font-medium text-mountbatten undefined"
-              >
-                Name
-              </label>
-              <div className="flex flex-col items-start">
-                <input
-                  type="text"
-                  name="name"
-                  className="block w-full mt-1 border-rusty rounded-md shadow-sm focus:border-auburn focus:ring focus:ring-rusty"
-                />
-              </div>
-            </div>
-            <div className="mt-4">
-              <label
-                htmlFor="email"
-                className="block text-sm font-medium text-mountbatten undefined"
-              >
-                Email
-              </label>
-              <div className="flex flex-col items-start">
-                <input
-                  type="email"
-                  name="email"
-                  className="block w-full mt-1 border-rosy rounded-md shadow-sm focus:border-rusty focus:ring focus:ring-rusty"
-                />
-              </div>
-            </div>
-            <div className="mt-4">
-              <label
-                htmlFor="password"
-                className="block text-sm font-medium text-mountbatten undefined"
-              >
-                Password
-              </label>
-              <div className="flex flex-col items-start">
-                <input
-                  type="password"
-                  name="password"
-                  className="block w-full mt-1 border-rusty rounded-md shadow-sm focus:border-indigo-300 focus:ring focus:ring-indigo-200 focus:ring-opacity-50"
-                />
-              </div>
-            </div>
-            <div className="mt-4">
-              <label
-                htmlFor="password_confirmation"
-                className="block text-sm font-medium text-mountbatten undefined"
-              >
-                Confirm Password
-              </label>
-              <div className="flex flex-col items-start">
-                <input
-                  type="password"
-                  name="password_confirmation"
-                  className="block w-full mt-1 border-rusty rounded-md shadow-sm focus:border-rusty focus:ring focus:ring-rusty"
-                />
-              </div>
-            </div>
-            <a href="#" className="text-xs text-rose hover:underline">
-              Forget Password?
-            </a>
-            <div className="flex items-center mt-4">
-              <button className="w-full px-4 py-2 tracking-wide text-misty transition-colors duration-200 transform bg-auburn rounded-md hover:bg-rosy focus:outline-none focus:bg-rusty">
-                Register
-              </button>
-            </div>
-          </form>
-          <div className="mt-4 text-mountbatten">
-            Already have an account?
-            <span>
-              <Link className="text-rusty hover:underline" to="/login">
-                Log in
-              </Link>
-            </span>
-          </div>
-          <div className="flex items-center w-full my-4">
-            <hr className="w-full" />
-            <p className="px-3 text-mountbatten">OR</p>
-            <hr className="w-full" />
-          </div>
+          <img className="mx-auto h-12 w-auto" src={logo} alt="anikaze logo" />
+          <h2 className="mt-6 text-center text-3xl font-bold tracking-tight text-auburn">
+            Create Account
+          </h2>
+          <p className="text-mountbatten text-center">
+            Already have an account?{" "}
+            <Link to="/login" className="text-auburn">
+              Sign in
+            </Link>
+          </p>
           <div className="my-6 space-y-2">
             <button
               aria-label="Login with Google"
               type="button"
-              className="text-mountbatten flex items-center justify-center w-full p-2 space-x-4 border rounded-md focus:ring-2 focus:ring-offset-1 dark:border-rusty focus:ring-rusty"
+              className="text-mountbatten flex items-center justify-center w-full p-2 space-x-4 border rounded-md dark:border-rusty hover:bg-misty"
             >
               <svg
                 xmlns="http://www.w3.org/2000/svg"
                 viewBox="0 0 32 32"
-                className="w-5 h-5 fill-current"
+                className="w-5 h-5 fill-[#D54D44]"
               >
                 <path d="M16.318 13.714v5.484h9.078c-0.37 2.354-2.745 6.901-9.078 6.901-5.458 0-9.917-4.521-9.917-10.099s4.458-10.099 9.917-10.099c3.109 0 5.193 1.318 6.38 2.464l4.339-4.182c-2.786-2.599-6.396-4.182-10.719-4.182-8.844 0-16 7.151-16 16s7.156 16 16 16c9.234 0 15.365-6.49 15.365-15.635 0-1.052-0.115-1.854-0.255-2.651z"></path>
               </svg>
-              <p>Login with Google</p>
+              <p>Sign up with google</p>
             </button>
           </div>
+
+          <div className="inline-flex items-center justify-center w-full">
+            <hr className="w-64 h-1 my-8  border-0 rounded solid dark:bg-auburn" />
+            <div className="absolute px-4 -translate-x-1/2 bg-[#EEDEDD] left-1/2 ">
+              <p className="text-xl text-auburn">or</p>
+            </div>
+          </div>
         </div>
+        <form className="mt-8 space-y-6">
+          <div className="rounded-md shadow-sm">
+            <div>
+              <p>Name</p>
+              <input
+                id="name"
+                name="name"
+                type="name"
+                required
+                className="relative block w-full rounded-t-md border-0 py-1.5 indent-[5px] text-mountbatten placeholder:text-rose sm:text-sm sm:leading-6"
+                placeholder="Ex.: Natalia Carvalho"
+              />
+            </div>
+            <div>
+              <p className="mx-[1px] my-[6px]">Email</p>
+              <input
+                id="email-address"
+                name="email"
+                type="email"
+                required
+                className="relative block w-full rounded-b-md border-0 py-1.5 text-night ring-1 ring-inset indent-[5px] ring-gray-300 placeholder:text-mountbatten sm:text-sm sm:leading-6"
+                placeholder="Ex.: email@email.com"
+              />
+            </div>
+
+            <div>
+              <p className="mx-[1px] my-[6px]">Password</p>
+              <label className="sr-only">Password</label>
+              <input
+                id="password"
+                name="password"
+                type="password"
+                required
+                className="relative block w-full rounded-b-md border-0 py-1.5 text-night ring-1 ring-inset indent-[5px] ring-gray-300 placeholder:text-mountbatten sm:text-sm sm:leading-6"
+                placeholder="Password"
+              />
+            </div>
+          </div>
+
+          <div>
+            <button
+              type="submit"
+              className="group relative flex w-full justify-center rounded-md bg-[#D54D44] py-2 px-3 text-sm font-semibold text-white hover:bg-rosy"
+            >
+              Sign in
+            </button>
+          </div>
+        </form>
       </div>
-      <Power />
-      <Footer />
-    </>
+    </section>
   );
 }
 
